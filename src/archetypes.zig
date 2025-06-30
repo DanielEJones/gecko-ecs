8adbeb092c89e1476ea4999ce3632c780ec3f7d5
DanielEJones <djonesthe21st@gmail.com>
2025-06-30 16:29:16 +0800

    implemented archetype iterators

--- src/archetypes.zig
+++ src/archetypes.zig
@@ -6,13 +6,13 @@ const comps = @import("components.zig");
 const Erased = comps.ErasedComponentStore;
 const Store = comps.ComponentStore;
 
-pub const Archetpye = struct {
+pub const Archetype = struct {
     components: Map(Erased),
     field_count: usize,
     size: usize,
 
     /// Create a new archetype from the given fields
-    pub fn init(allocator: Allocator, fields: anytype) !Archetpye {
+    pub fn init(allocator: Allocator, fields: anytype) !Archetype {
         var map: Map(Erased) = .{};
         errdefer map.deinit(allocator);
         var size: usize = 0;
@@ -32,14 +32,14 @@ pub const Archetpye = struct {
     }
 
     /// Delete an archetype and it's underlying storages
-    pub fn deinit(self: *Archetpye, allocator: Allocator) void {
+    pub fn deinit(self: *Archetype, allocator: Allocator) void {
         var components = self.components.valueIterator();
         while (components.next()) |component| component.deinit(allocator);
         self.components.deinit(allocator);
     }
 
     /// Add a new element to the archetype, consturcted from the given fields
-    pub fn add(self: *Archetpye, allocator: Allocator, fields: anytype) !void {
+    pub fn add(self: *Archetype, allocator: Allocator, fields: anytype) !void {
         var count: usize = 0;
 
         // if for whatever reason we error out, go through and strip
@@ -66,7 +66,64 @@ pub const Archetpye = struct {
         if (count != self.components.count()) return error.MissingFields;
         self.size += 1;
     }
+
+    /// Get an iterator over a subset of the components
+    pub fn view(self: *Archetype, comptime T: type) !ArchetypeIterator(T) {
+        // ensure that all the fields actually exist
+        inline for (@typeInfo(T).@"struct".fields) |field| {
+            switch (@typeInfo(field.type)) {
+                .pointer => |ptr| {
+                    if (self.components.get(@typeName(ptr.child)) == null)
+                        return error.FieldNotFound;
+                },
+                .@"struct" => {
+                    if (self.components.get(@typeName(field.type)) == null)
+                        return error.FieldNotFound;
+                },
+                else => {},
+            }
+        }
+
+        return ArchetypeIterator(T).init(self);
+    }
 };
+
+pub fn ArchetypeIterator(comptime T: type) type {
+    return struct {
+        archetype: *Archetype,
+        index: usize,
+
+        pub fn init(archetype: *Archetype) ArchetypeIterator(T) {
+            return .{ .archetype = archetype, .index = 0 };
+        }
+
+        pub fn next(self: *ArchetypeIterator(T)) ?T {
+            if (self.index >= self.archetype.size) return null;
+
+            var result: T = undefined;
+
+            inline for (@typeInfo(T).@"struct".fields) |field| {
+                switch (@typeInfo(field.type)) {
+                    .pointer => |ptr| {
+                        const Underlying = ptr.child;
+                        var store = self.archetype.components.get(@typeName(Underlying)).?;
+                        @field(result, field.name) = store.as(Underlying).get(self.index).?;
+                    },
+
+                    .@"struct" => {
+                        var store = self.archetype.components.get(@typeName(field.type)).?;
+                        @field(result, field.name) = store.as(field.type).get(self.index).?.*;
+                    },
+
+                    else => {},
+                }
+            }
+
+            self.index += 1;
+            return result;
+        }
+    };
+}
 
 // +----------------------------------------------------------------------------------------------
 // | Tests
@@ -86,7 +143,7 @@ test "archetpye can be created" {
     const alloc = gpa.allocator();
     defer _ = gpa.deinit();
 
-    var arch = try Archetpye.init(alloc, .{ Position, Velocity });
+    var arch = try Archetype.init(alloc, .{ Position, Velocity });
     defer arch.deinit(alloc);
 
     { // add a position and a velocity
@@ -114,7 +171,7 @@ test "adding new elements works" {
     const alloc = gpa.allocator();
     defer _ = gpa.deinit();
 
-    var arch = try Archetpye.init(alloc, .{ Position, Velocity });
+    var arch = try Archetype.init(alloc, .{ Position, Velocity });
     defer arch.deinit(alloc);
 
     std.testing.log_level = .debug;
@@ -150,4 +207,40 @@ test "adding new elements works" {
         try expect(arch.size == 1);
         try expect(position.as(Position).get(1) == null);
     }
+}
+
+// +-- Archetype Iterator Tests ------------------------------------------------------------------
+
+test "can iterate over component" {
+    // make an allocator to test with
+    var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
+    const alloc = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    var moveable = try Archetype.init(alloc, .{ Position, Velocity });
+    defer moveable.deinit(alloc);
+
+    try moveable.add(alloc, .{ Position{ .x = 10, .y = 11 }, Velocity{ .vx = 12, .vy = 13 } });
+    try moveable.add(alloc, .{ Position{ .x = 14, .y = 15 }, Velocity{ .vx = 16, .vy = 17 } });
+    try moveable.add(alloc, .{ Position{ .x = 18, .y = 19 }, Velocity{ .vx = 20, .vy = 21 } });
+
+    var it = try moveable.view(struct { p: *Position, v: Velocity });
+
+    try expect(it.next().?.p.x == 10);
+    try expect(it.next().?.v.vx == 16);
+    try expect(it.next().?.p.y == 19);
+    try expect(it.next() == null);
+}
+
+test "iterators can fail" {
+    // make an allocator to test with
+    var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
+    const alloc = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    var moveable = try Archetype.init(alloc, .{ Position, Velocity });
+    defer moveable.deinit(alloc);
+
+    const T = struct { bad: u8 };
+    try fails(error.FieldNotFound, moveable.view(struct { bad: T }));
 }
